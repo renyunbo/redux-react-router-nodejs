f15c2cfd4a7c4641f4de54b65e0aae1b6d973c93
renyb <renyb@workai.com.cn>
2019-07-02 14:42:27 +0800

    添加调试工具

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {createStore,applyMiddleware} from 'redux';//applyMiddleware处理中间件  (处理异步)
+import { createStore, applyMiddleware, compose } from 'redux';//applyMiddleware处理中间件  (处理异步)
 import thunk from 'redux-thunk';
 import App from './App';
-import {counter,addNum,removeNum,addNumAsync} from './index.redux';
-import { create } from 'handlebars';
-import { from } from 'rxjs';
+import { counter, addNum, removeNum, addNumAsync } from './index.redux';
+
 //redux和react相连接
 // 1.新建store
-const store = createStore(counter,applyMiddleware(thunk));//createStore里面需要传递一个reducer
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f=>f
+));
 
-function render(){
+
+// const store = createStore(counter, applyMiddleware(thunk));//createStore里面需要传递一个reducer
+
+function render() {
     //2.store已组件属性的方式传递进去
-    ReactDom.render(<App store={store} addNum={addNum} removeNum={removeNum} addNumAsync={addNumAsync}/>,document.getElementById('root'))
+    ReactDom.render(<App store={store} addNum={addNum} removeNum={removeNum} addNumAsync={addNumAsync} />, document.getElementById('root'))
 }
 render();
 //3外界已subscribe的形式订阅render函数，这样状态有什么变化，render会重新执行
